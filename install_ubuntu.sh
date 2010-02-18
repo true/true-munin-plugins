1a87eb2318646631fd92cc6481c4560f9e3916aa
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-02-18 14:02:50 +0100

    libvirt placeholder

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+set -x
 
+function getdirs() {
+    wget -qO- "http://github.com/true/true-munin-plugins${1}?raw=true" |html2text |egrep '/$' |egrep -v '^_' |sed 's#[^a-z0-9A-Z_]##g'
+}
+
+apt-get -fym install html2text dialog
+
+list=""
+for dir in $(getdirs '/'); do
+    status="on"
+    if [ "${dir}" = "libvirt" ]; then
+        status="off"
+    fi
+
+    list="${list} ${dir} ${dir} ${status}"
+done
+
+dialog --checklist "Which plugins do you want to install?" 20 40 10 ${list}  2> /tmp/answer$$
+plugins=$(cat /tmp/answer$$)
+
+for plugin in ${plugins}; do
+    echo $plugin;
+    # /usr/share/munin/plugins/
+
+
+done
 
 
 
